80da7d55664c99bbf61dffe41f6c8d025783ce01
Mohamed Rikaz <mohamedrks@gmail.com>
2022-08-16 14:03:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 
 @app.route('/cal/', methods=['GET'])
 def home_page():
-    data_set = {'page' : 'Home' , 'Time' : time.time()} 
+    data_set = {'page' : 'Home' , 'Time' : time.time()}
+    
     user_query = str(request.args.get('temp'))
     temp = float(user_query)
+    
     result = pmv_ppd(tdb=27, tr=temp , vr=0.1, rh=50, met=1.1, clo=0.5, standard="ASHRAE")
 
     json_dump = json.dumps(result)
