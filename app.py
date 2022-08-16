81e7424391628c9da6b17e6d6d89422813ec01b8
Rikas <mohamedrks@gmail.com>
2022-08-16 13:29:26 +0100

    added parameters

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ import json, time
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/cal/', methods=['GET'])
 def home_page():
     data_set = {'page' : 'Home' , 'Time' : time.time()} 
-    result = pmv_ppd(tdb=27, tr=27 , vr=0.1, rh=50, met=1.1, clo=0.5, standard="ASHRAE")
+    user_query = str(request.args.get('temp'))
+    temp = float(user_query)
+    result = pmv_ppd(tdb=27, tr=temp , vr=0.1, rh=50, met=1.1, clo=0.5, standard="ASHRAE")
 
     json_dump = json.dumps(result)
 
     return json_dump
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run()
